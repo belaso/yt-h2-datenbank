035f442f9c6b9afb70f0c6b020266003ab5e8da9
Christian Trutz <info@christian-trutz.de>
2019-01-12 21:49:15 +0100

    H2 Console programmatisch aufrufen

--- src/main/java/h2test/H2Tests.java
+++ src/main/java/h2test/H2Tests.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.h2.tools.Server;
+
 public class H2Tests {
 
 	public static void main(String[] args) throws SQLException {
@@ -11,6 +13,7 @@ public class H2Tests {
 		connection.createStatement().execute("drop table if exists person");
 		connection.createStatement().execute("create table person(nachname varchar(100))");
 		connection.createStatement().execute("insert into person(nachname) values('Trutz')");
+		Server.startWebServer(connection);
 		connection.close();
 	}
 
