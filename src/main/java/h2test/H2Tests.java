e7e62e9186bb324903014b14c901bd23ff64df76
Christian Trutz <info@christian-trutz.de>
2019-01-12 20:28:51 +0100

    Datenbank Connection mit H2 erstellt

---
+++ src/main/java/h2test/H2Tests.java
@@ -0,0 +1,17 @@
+package h2test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class H2Tests {
+
+	public static void main(String[] args) throws SQLException {
+		Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+		connection.createStatement().execute("drop table if exists person");
+		connection.createStatement().execute("create table person(nachname varchar(100))");
+		connection.createStatement().execute("insert into person(nachname) values('Trutz')");
+		connection.close();
+	}
+
+}
