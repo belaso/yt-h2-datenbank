632b53933e4cadbacfaf9b0e6adc49f41e639b34
Christian Trutz <info@christian-trutz.de>
2019-01-13 11:20:32 +0100

    Video beendet

---
+++ src/main/java/h2test/H2Tests.java
@@ -0,0 +1,19 @@
+package h2test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
+
+public class H2Tests {
+
+	public static void main(String[] args) throws SQLException {
+		Connection connection = DriverManager.getConnection("jdbc:h2:mem:test", "sa", "");
+		connection.createStatement().execute("create table person(nachname varchar(100))");
+		connection.createStatement().execute("insert into person(nachname) values('Trutz')");
+		Server.startWebServer(connection);
+		connection.close();
+	}
+
+}
